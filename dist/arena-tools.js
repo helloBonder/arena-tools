fae6d7e2cb896812c74c6ac2b3209e287c31f1c2
Conrado <conco@192.168.1.3>
2022-10-20 14:28:49 -0300

    api

--- dist/arena-tools.js
+++ dist/arena-tools.js
@@ -4,6 +4,26 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.logAccountConnectedEvent = logAccountConnectedEvent;
-function logAccountConnectedEvent(dApp, walletAddress) {
-  console.log('holis');
+require("core-js/modules/es.promise.js");
+require("core-js/modules/es.json.stringify.js");
+const EVENT_CONNECTION_PATH = '/event/connect';
+const API_URL = 'http://localhost:8080/';
+async function logAccountConnectedEvent(dApp, walletAddress) {
+  if (!dApp || !walletAddress) return false;
+  const reqBody = {
+    'dApp': dApp,
+    'address': walletAddress
+  };
+  const reqOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reqBody)
+  };
+  fetch(API_URL + EVENT_CONNECTION_PATH, reqOptions).then(res => {
+    console.log(res);
+  }).catch(error => {
+    console.log(error);
+  });
 }
--- src/lib/arena-tools.js
+++ src/lib/arena-tools.js
@@ -1,6 +1,26 @@
+const EVENT_CONNECTION_PATH = '/event/connect'
+const API_URL = 'http://localhost:8080/'
 
-function logAccountConnectedEvent(dApp, walletAddress) {
-    console.log('holis')
+async function logAccountConnectedEvent(dApp, walletAddress) {
+    if (!dApp || !walletAddress) return false;
+    const reqBody = {
+        'dApp': dApp,
+        'address': walletAddress
+    };
+    const reqOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            },
+        body: JSON.stringify(reqBody),
+    };
+    console.log(API_URL + EVENT_CONNECTION_PATH)
+    fetch(API_URL + EVENT_CONNECTION_PATH, reqOptions)
+        .then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.error(error);
+        });
 }
 
 export { logAccountConnectedEvent }
