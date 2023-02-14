b91e7d09866aff70c258acd1572d85f368f84137
Fer Martin <feer.1711@gmail.com>
2023-02-14 12:18:17 -0300

    arena tools v1.0.0

---
+++ lib/arena-tools.d.ts
@@ -0,0 +1,2 @@
+declare function logEvent(token: any, walletAddress: any, event: any): Promise<boolean>;
+export { logEvent };
---
+++ src/arena-tools.ts
@@ -0,0 +1,27 @@
+const CUSTOM_EVENT_PATH = 'event/custom'
+const API_URL = 'https://api.getarena.xyz/'
+
+async function logEvent(token, walletAddress, event) {
+    if (!token || !event) return false;
+    const reqBody = {
+        'token': token,
+        'event': event,
+        'address': walletAddress
+    };
+    const reqOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(reqBody),
+    };
+    fetch(API_URL + CUSTOM_EVENT_PATH, reqOptions)
+        .then(res => {
+            return true
+        }).catch(error => {
+        console.error(error);
+        return false
+    });
+}
+
+export { logEvent }
